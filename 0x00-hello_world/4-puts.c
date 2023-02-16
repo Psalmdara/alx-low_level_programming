96ea82509470976c104ea3f71633a5147fd5e8a3
Psalmdara <dareomoge@gmail.com>
2023-02-16 09:09:07 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
-int main(void)
+/* Programming is like building a multilingual puzzle */
+
+init main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("Programming is like building a multilingual puzzle \n");
+	return(0);
 }
