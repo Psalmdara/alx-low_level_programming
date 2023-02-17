f685d819b34eb1528a81709085037e00dc0be676
Psalmdara <dareomoge@gmail.com>
2023-02-17 14:12:36 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int c;
 	int d = 0;
-	
+
 	while (d < 10)
 	{
 		c = 0;
@@ -16,15 +16,14 @@ int main(void)
 			if (d != c && d < c)
 			{
 				putchar('0' + d);
-				putchar('0' + c);
-				
+				putchar('0' + c)
+
 				if (c + d != 17)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			
 			c++;
 		}
 		d++;
