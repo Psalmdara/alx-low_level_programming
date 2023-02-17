3f5e69c7bed7b2728f6117816b4784ef50a87d8a
Psalmdara <dareomoge@gmail.com>
2023-02-17 14:19:04 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 			if (d != c && d < c)
 			{
 				putchar('0' + d);
-				putchar('0' + c)
+				putchar('0' + c);
 
 				if (c + d != 17)
 				{
@@ -24,6 +24,7 @@ int main(void)
 					putchar(' ');
 				}
 			}
+
 			c++;
 		}
 		d++;
