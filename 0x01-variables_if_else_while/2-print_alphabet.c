ab2415389302e32a334339d505921d4ae73c28e1
Psalmdara <dareomoge@gmail.com>
2023-02-17 09:49:33 -0800

    Print_alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * Main  - Program that prints the alphabets in lower case
- * followed by a line
- * Description: You can only use the putchar
+ * Main  - Program that prints the alphabets in lower case,
+ *         followed by a line
+ * You can only use the putchar twice
  * Return: 0
  */
 int main(void)
