9b41d6c1e59ade6ade9c143c601317a85c0f9083
Psalmdara <dareomoge@gmail.com>
2023-02-17 13:08:14 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+	char c = 'a';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
