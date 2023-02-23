adbea83fa85638b5736836d346e4e209f2a88ea9
Psalmdara <dareomoge@gmail.com>
2023-02-23 15:35:37 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * except 2 and 4, followed by a new line
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		while (i != 2 && i != 4)
+		{
+			_putchar (i + '0');
+		}
+		i++;
+	}
+
+	_putchar ('\n');
+}
