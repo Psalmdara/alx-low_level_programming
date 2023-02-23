6fb07d4bb816feeb74bce1350b996203dca2cb84
Psalmdara <dareomoge@gmail.com>
2023-02-23 15:43:39 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,12 @@ void print_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar (i + '0');
 		}
-		i++;
 	}
 
 	_putchar ('\n');
