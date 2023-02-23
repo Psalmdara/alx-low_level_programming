a945248b90de52ae496839bd361d1762161482e4
Psalmdara <dareomoge@gmail.com>
2023-02-23 06:09:51 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 
 /**
  * _isupper - checks for uppercase character
- * Returns 1 if c is uppercase
- * Returns 0 otherwise
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
