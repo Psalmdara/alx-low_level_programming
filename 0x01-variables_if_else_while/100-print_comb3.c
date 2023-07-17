c87d86577189c8855e42f3473626e5a33a34a974
psalmdara <dareomoge@gmail.com>
2023-07-17 11:54:00 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that print all possible different 
+ * combinations of two digit numbers separated by ,and a space
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 48;
+	int b;
+
+	while (a <= 57)
+	{
+		b = a + 1;
+		while (b <= 57)
+		{
+		putchar(a);
+		putchar(b);
+
+		if (a == 56 && b == 57)
+			{
+				putchar('\n');
+			}
+			else 
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+
+		b++;
+		}
+
+		a++;
+
+	}
+
+	return (0);
+}
