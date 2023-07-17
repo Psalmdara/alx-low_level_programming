56825a7977909104f75fedd1e40eafd7c5080111
psalmdara <dareomoge@gmail.com>
2023-07-17 08:43:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that print alphabets followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch >= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
