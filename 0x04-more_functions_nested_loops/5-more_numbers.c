323c17ba8d97b7f18683be74fe2b19a9775019c0
psalmdara <dareomoge@gmail.com>
2023-07-20 23:35:45 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,7 @@
  * more_numbers - Write a function that prints 10 times the
  * numbers, from 0 to 14, followed by a new line.
  *
- * Return: 0
+ * Return: void
  */
 
 void more_numbers(void)
@@ -21,8 +21,7 @@ void more_numbers(void)
 				_putchar((j / 10) + '0');
 			}
 			_putchar((j % 10) + '0');
-		}	
-	
+		}
 		_putchar('\n');
 	}
 }
